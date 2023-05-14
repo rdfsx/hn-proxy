5f07baf70bb014b16300af6256c1c9db59f30f22
Mikhail Anishchanka <m315498@gmail.com>
2023-05-14 15:29:04 +0300

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,42 @@
+import httpx as httpx
+from fastapi import FastAPI
+from starlette.requests import Request
+from starlette.responses import Response
+
+app = FastAPI()
+client = httpx.AsyncClient(base_url="https://news.ycombinator.com/")
+
+
+@app.api_route("/{path_name:path}", methods=["GET"])
+async def perform_proxy(request: Request):
+    proxy = await client.get(
+        httpx.URL(path=request.url.path, query=request.url.query.encode("utf-8"))
+    )
+    script = bytes(
+        r"""<script>
+    function replaceInText(element, pattern, replacement) {
+        for (let node of element.childNodes) {
+            switch (node.nodeType) {
+                case Node.ELEMENT_NODE:
+                    replaceInText(node, pattern, replacement);
+                    break;
+                case Node.TEXT_NODE:
+                    node.textContent = node.textContent.replace(pattern, replacement);
+                    break;
+                case Node.DOCUMENT_NODE:
+                    replaceInText(node, pattern, replacement);
+            }
+        }
+    }
+    replaceInText(document.body, /(?!\/)\b[a-zA-Z]{6}\b(?!\/)/g, matched => matched + "\u2122")
+    </script>""",
+        "utf-8",
+    )
+    if "text/html" in proxy.headers["content-type"]:
+        idx = proxy.content.index(b"</html>")
+        content = proxy.content[:idx] + script + proxy.content[idx:]
+    else:
+        content = proxy.content
+    return Response(
+        content, proxy.status_code, {"content-type": proxy.headers["Content-Type"]}
+    )
